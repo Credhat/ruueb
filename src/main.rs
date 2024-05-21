cd5281f54d48febd8e8e53efb3267ef0069e6a91
unic <2445942195@qq.com>
2024-05-21 16:01:32 +0800

    🚝customize the sleep time by /sleep[time] router command

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,19 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let (header, html_file) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
         "GET /sleep HTTP/1.1" => {
-            thread::sleep(time::Duration::from_secs(2));
+            thread::sleep(time::Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "index.html")
         }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        value => {
+            if value.contains("GET /sleep") && value.contains("HTTP/1.1") {
+                let mut parts = value.split_ascii_whitespace();
+                let sleep_time: u64 = parts.nth(1).unwrap().replace("/sleep", "").parse().unwrap();
+                thread::sleep(time::Duration::from_secs(sleep_time.min(10)));
+                ("HTTP/1.1 200 OK", "index.html")
+            } else {
+                ("HTTP/1.1 404 NOT FOUND", "404.html")
+            }
+        }
     };
 
     html_path.push(html_file);
